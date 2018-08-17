d365bf7e96b5f028ca20663b64c9082b94dfb447
Jason Viger <jason.viger@gmail.com>
2018-08-17 11:31:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,24 +5,42 @@ class Shoe
     @brand = brand
   end
   
-  def brand=
-    @brand
+  def brand=(brand)
+    @brand = brand
   end
   
   def brand
     @brand
   end
   
+  def color=(color) 
+    @color = color
+  end
+  
   def color 
     @color
+  end
+  
+  
+  def size=(size)
+    @size = size
   end
   
   def size 
     @size
   end
   
+  def condition=(condition)
+    @condition = condition
+  end
+  
   def condition 
     @condition
+  end
+  
+  
+  def material=(material)
+    @material = material
   end
   
   def material 
