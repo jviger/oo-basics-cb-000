856776b36033a0c5b0655ac801517a48d0f809c9
Jason Viger <jason.viger@gmail.com>
2018-08-17 11:25:23 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,21 @@
 # Make your shoe class here!
 class Shoe 
   
+  def initialize(title)
+    @title = title
+  end
   
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
   
   
   
